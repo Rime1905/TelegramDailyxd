b209f5871c151f78867a02586ec87ee3d5137b99
Rime1905 <99634100+Rime1905@users.noreply.github.com>
2022-05-22 21:38:28 +0600

    Add files via upload

---
+++ configer.py
@@ -0,0 +1,9 @@
+token_pyow = 'a01bff19c34db14ebf253e1a3514b7c5' #Токен погоды - https://openweathermap.org/
+
+token_tg = '5102346875:AAGTNsoNbg4ASb6uDvnC9Z_PIeNMI7f6y98' #Токен телеграм бота
+
+token_money = "cc14efd64f8c442681bca73dd74ac40c" #Токен валют - https://openexchangerates.org/
+
+token_newsss ='b33df95ebba74f33892a2b583a7313e9' #Токен новостей - https://newsapi.org/
+
+token_imbb = '4574c73620e082ee4c01b3ad34914cdd' #Токен загрузки изображений
